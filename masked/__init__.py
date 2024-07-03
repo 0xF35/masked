c61e514bef896dd2aa634cf6f8368a51748f4bfb
Dhruval Parmar <77436973+0xF35@users.noreply.github.com>
2024-07-03 11:21:59 +0530

    Update __init__.py

--- masked/__init__.py
+++ masked/__init__.py
@@ -1,3 +1,7 @@
+__author__ = 'Dhruval Parmar'
+__version__ = '1.0.0'
+__email__ = 'dhruvalparmar@duck.com'
+
 import msvcrt
 
 def mask_input(p, mask_with='•'):
@@ -9,7 +13,7 @@ def mask_input(p, mask_with='•'):
 			if char == '\r':break
 			elif char == '\x08':
 				text = text[:-1]
-				print(f'\r{p}{mask_with*(char_count - 1) + " "}', end='')
+				print(f'\r{p}{mask_with*(char_count - 1) + " "}' if char_count > 0 else f'\r{p}{mask_with*(char_count - 1)}', end='')
 				char_count -= 1 if char_count > 0 else 0
 			else:
 				text+=char
